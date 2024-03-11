98373a4a66047cfe2fd977f159b6494902826a8b
Miriam Curiel <curiel.miriam@gmail.com>
2024-03-11 14:16:29 -0600

    obteniendo info de mi servicio

---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, of } from 'rxjs';
+
+import { Hero } from '../interfaces/hero.interface';
+import { environments } from '../../../environments/environments';
+
+@Injectable({ providedIn: 'root' })
+export class HeroesService {
+
+  // Crear constante de manera privada que
+  // viene desde mi enviroments
+  private baseUrl: string = environments.baseUrl;
+
+
+  constructor(private http: HttpClient) { }
+
+
+  getHeroes():Observable<Hero[]> {
+    // Uso la peticion get y le asigno el path
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  }
+
+  // getHeroById( id: string ): Observable<Hero|undefined> {
+  //   return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+  //     .pipe(
+  //       catchError( error => of(undefined) )
+  //     );
+  // }
+
+  // getSuggestions( query: string ): Observable<Hero[]> {
+  //   return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+  // }
+
+
+
+}
