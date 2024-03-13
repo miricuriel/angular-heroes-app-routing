03f0c5a11161a4f7bc80088eb03a561fb7e3e5d6
Miriam Curiel <curiel.miriam@gmail.com>
2024-03-13 11:46:59 -0600

    Buscador

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -28,11 +28,9 @@ export class HeroesService {
         catchError( error => of(undefined) )
       );
   }
-
-  // getSuggestions( query: string ): Observable<Hero[]> {
-  //   return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
-  // }
-
-
+// Regresa un observable de tipo hero
+  getSuggestions( query: string ): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+  }
 
 }
